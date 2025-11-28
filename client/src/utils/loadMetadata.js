57c6087b32ffa3278d3f9627a523e7c03531fd69
olkaniecki <olivialkaniecki@gmail.com>
2025-11-28 11:05:10 -0500

    Fix links

--- client/src/utils/loadMetadata.js
+++ client/src/utils/loadMetadata.js
@@ -1,24 +1,20 @@
 import { useEffect, useState } from "react";
 
 export function useBuildingMetadata() {
-    // metadata --> will hold the array of building metadata from JSON
     const [metadata, setMetadata] = useState({});
 
-    // performs fetch request to load metadata
     useEffect(() => {
         async function loadMetadata() {
             try {
-                const res = await fetch("/metadata/metadata.json");
+                const res = await fetch(process.env.PUBLIC_URL + "/metadata/metadata.json");
                 const data = await res.json();
                 setMetadata(data);
             } catch (err) {
-                // if something went wrong, log error
                 console.error("Error loading metadata:", err);
-            } 
+            }
         }
         loadMetadata();
     }, []);
     
-    // return metadata 
     return metadata;
 }
