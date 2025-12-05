32fd9c9dfa6f59810191810e0a836fd3be5fddb1
Nick Pajak <232944112+NickPajak@users.noreply.github.com>
2025-12-05 10:42:12 -0500

    improved sidebar scrolling behavior and anchored route start and reset buttons

--- client/src/components/RoutePlanner.jsx
+++ client/src/components/RoutePlanner.jsx
@@ -1,8 +1,8 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import styled from "styled-components";
 import RouteSearchBar from './RouteSearchBar';
 import DestinationCard from './DestinationCard';
-import {MapPinIcon, HomeIcon} from '@heroicons/react/24/solid';
+import { MapPinIcon, HomeIcon } from '@heroicons/react/24/solid';
 import { Icon } from 'leaflet';
 
 const CardRow = styled.div`
@@ -34,6 +34,34 @@ const WelcomeText = styled.h2`
     color: ${props => props.theme.routePlannerText};
 `;
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  position: relative;
+`;
+
+const ScrollArea = styled.div`
+  flex: 1;
+  overflow-y: auto;
+  padding-bottom: 50px; /* ensures content shouldnt hide behind bottombar */
+  padding-right: 5px;
+`;
+
+const BottomBar = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 12px 16px;
+  background: ${props => props.theme.bottomBarColor || "white"};
+  display: flex;
+  gap: 12px;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 2;
+`;
+
 const StartRouteButton = styled.button`
     background-color: #fdb515;
     color: black;
@@ -41,7 +69,6 @@ const StartRouteButton = styled.button`
     border: none;
     cursor: pointer;
     padding: 3px 20px;
-    margin-top: 24px; 
 
     &:hover {
         background-color: #3b3b3b;
@@ -62,7 +89,7 @@ const MapIcon = styled(MapPinIcon)`
     color: ${props => props.theme.startIconColor};
 `;
 
-export default function RoutePlanner( {onSelectFeature, addFeature, onFeatureConsumed, onRouteRequest, onExpand, onCollapse} ) {
+export default function RoutePlanner({ onSelectFeature, addFeature, onFeatureConsumed, onRouteRequest, onExpand, onCollapse }) {
     // Track which step the user is on: "start", "end", or "done"
     const [step, setStep] = useState("start");
 
@@ -82,7 +109,7 @@ export default function RoutePlanner( {onSelectFeature, addFeature, onFeatureCon
         } else if (step === "end") {
             // Don't allow selecting the same building as start
             if (startDestination && building.properties.building_id === startDestination.properties.building_id) {
-            return; // ignore duplicate
+                return; // ignore duplicate
             }
             setEndDestination(building);
             setStep("done");
@@ -114,8 +141,9 @@ export default function RoutePlanner( {onSelectFeature, addFeature, onFeatureCon
     }, [addFeature, handleSelectBuilding, onFeatureConsumed, startDestination, endDestination]);
 
 
-    return(
-        <div style={{ position: "relative", paddingBottom:"80px"}}>
+    return (
+        <Wrapper>
+            {/* <div style={{ position: "relative", paddingBottom: "80px" }}> */}
             <Title>Campus Compass</Title>
             <RouteSearchBar
                 key={step}
@@ -125,113 +153,115 @@ export default function RoutePlanner( {onSelectFeature, addFeature, onFeatureCon
                 onSelectBuilding={handleSelectBuilding}
                 disabledBuilding={step === "end" ? startDestination : null}
                 onFocus={onExpand}
-                onBlur={() => {}}
+                onBlur={() => { }}
             />
 
-
-            {/* if there's no place added to route, add default welcome text*/}
-            {!startDestination && !endDestination && (
-                <WelcomeText>
-                    <div
-                        style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        height: "200px", 
-                        backgroundColor: "rgba(253, 180, 21, 0)", 
-                        borderRadius: "12px",
-                        marginBottom: "16px",
-                        textAlign: "center",
-                        fontSize: "1.2rem",
-                        fontWeight: "700",
-                        gap: "10px",
-                        }}
-                    >
-                        <MapPinIcon style={{width: '50px', height: '50px', color: '#fdb515'}} />
-                        <div>Your path, made easier. </div>
-                        <div>Begin by adding your current location. </div>
-                    </div>
-                </WelcomeText>
-            )}
-
-            {/* Show both cards stacked below */}
-            <div style={{ marginTop: "16px", position: "relative" }}>
-                {startDestination && (
-                    <CardRow>
-                        <IconWrapper style={{ marginTop: "8px"}}>
-                            <StartIcon />
-                        </IconWrapper>
-                        
-                <DestinationCard
-                    label="Start"
-                    building={startDestination}
-                />
-                </CardRow>
+            <ScrollArea>
+                {/* if there's no place added to route, add default welcome text*/}
+                {!startDestination && !endDestination && (
+                    <WelcomeText>
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "center",
+                                justifyContent: "center",
+                                height: "200px",
+                                backgroundColor: "rgba(253, 180, 21, 0)",
+                                borderRadius: "12px",
+                                marginBottom: "16px",
+                                textAlign: "center",
+                                fontSize: "1.2rem",
+                                fontWeight: "700",
+                                gap: "10px",
+                            }}
+                        >
+                            <MapPinIcon style={{ width: '50px', height: '50px', color: '#fdb515' }} />
+                            <div>Your path, made easier. </div>
+                            <div>Begin by adding your current location. </div>
+                        </div>
+                    </WelcomeText>
                 )}
 
-                {endDestination && (
-                    <>  
+                {/* Show both cards stacked below */}
+                <div style={{ marginTop: "16px", position: "relative" }}>
+                    {startDestination && (
                         <CardRow>
-                            <IconWrapper>
-                                <MapIcon />
+                            <IconWrapper style={{ marginTop: "8px" }}>
+                                <StartIcon />
                             </IconWrapper>
+
                             <DestinationCard
-                            label="End"
-                            building={endDestination}
+                                label="Start"
+                                building={startDestination}
                             />
                         </CardRow>
-                    </>
-                )}
-            </div>
+                    )}
 
+                    {endDestination && (
+                        <>
+                            <CardRow>
+                                <IconWrapper>
+                                    <MapIcon />
+                                </IconWrapper>
+                                <DestinationCard
+                                    label="End"
+                                    building={endDestination}
+                                />
+                            </CardRow>
+                        </>
+                    )}
+                </div>
+            </ScrollArea>
+
+            {/* fixed bottom bar */}
             {(startDestination || endDestination) && (
-                <button
-                onClick={resetRoute}
-                style={{
-                    marginTop: "12px",
-                    padding: "8px 12px",
-                    backgroundColor: "#444",
-                    color: "white",
-                    border: "none",
-                    borderRadius: "8px",
-                    cursor: "pointer",
-                }}
-                >
-                Reset
-                </button>
+                <BottomBar>
+                    <button
+                        onClick={resetRoute}
+                        style={{
+                            padding: "10px 16px",
+                            backgroundColor: "#444",
+                            color: "white",
+                            border: "none",
+                            borderRadius: "1rem",
+                            cursor: "pointer",
+                            flex: 1,
+                        }}
+                    >
+                        <h3 style={{ margin: 0 }}>Reset</h3>
+                    </button>
+
+                    {/*TODO: Integrate button with route planning*/}
+                    {startDestination && endDestination && (
+                        <StartRouteButton
+                            style={{
+                                flex: 1,
+                                padding: "10px 16px",
+                            }}
+                            onClick={() => {
+                                if (startDestination && endDestination) {
+                                    onRouteRequest(
+                                        startDestination.properties.building_id || startDestination.properties.id,
+
+                                        endDestination.properties.building_id || endDestination.properties.id,
+                                    );
+                                    console.log("START ROUTE:",
+                                        startDestination?.properties?.building_id,
+                                        startDestination?.properties?.id,
+                                        endDestination?.properties?.building_id,
+                                        endDestination?.properties?.id
+                                    );
+                                }
+                            }}
+                        >
+                            <h3 style={{ margin: 0 }}>Start Route</h3>
+                        </StartRouteButton>
+                    )}
+                </BottomBar>
             )}
-            {/*TODO: Integrate button with route planning*/}
-            {startDestination && endDestination && (
-            <StartRouteButton 
-            style={{
-                position: "absolute",
-                bottom: "20px",
-                left: "50%",
-                transform: "translateX(-50%)",
-            }}
-
-            onClick={ () => {
-                if (startDestination && endDestination) {
-                    onRouteRequest(
-                        startDestination.properties.building_id || startDestination.properties.id,
-
-                        endDestination.properties.building_id || endDestination.properties.id,
-                    );
-                    console.log("START ROUTE:",
-                        startDestination?.properties?.building_id,
-                        startDestination?.properties?.id,
-                        endDestination?.properties?.building_id,
-                        endDestination?.properties?.id
-                    );
-                }
-            }}
-            >
-
-    <h3>Start Route</h3>
-</StartRouteButton>
-            )}
-        </div>
+        </Wrapper>
+        // </div>
     );
 }
 
